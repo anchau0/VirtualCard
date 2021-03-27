48a2f721eff932cbbc45e9d5cc238cbfdea8d479
aUsefulMethod <aUsefulMethod@gmail.com>
2021-03-26 22:41:12 -0500

    Basic Sign-Up fields with field-validation

--- VirtualProfessionalCard/Controllers/HomeController.cs
+++ VirtualProfessionalCard/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VirtualCard.Models;
 
 namespace VirtualCard.Controllers
 {
@@ -26,5 +27,27 @@ namespace VirtualCard.Controllers
 
             return View();
         }
+
+        public ActionResult SignUp()
+        {
+            ViewBag.Message = "Customer Sign Up";
+
+            return View();
+        }
+
+        [HttpPost] // is this like overloading a method? clearly an API call though?
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(CustomerModel model)
+        {
+            if (ModelState.IsValid) // extra security? extra-validation that all the critera we added to our model class is authentic
+            {
+
+                return RedirectToAction("Index");
+
+            }
+
+            return View(); 
+        }
+
     }
 }
---
+++ VirtualProfessionalCard/Models/CustomerModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; // this package allows us to add all the field validation below
+
+namespace VirtualCard.Models
+{
+    public class CustomerModel
+    {
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Enter a valid first Name.")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Enter a valid last Name.")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Please enter a valid email address")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Confirm Email")]
+        [Compare("EmailAddress", ErrorMessage = "The Email and Confirm Email must match.")]
+        public string ConfirmEmail { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "You must have a password.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "Your password needs to be between the lengths of 10 and 100")]
+        public string Password { get; set; }
+        
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Your passwords do not match!")]
+        public string ConfirmPassword { get; set; }
+        
+
+    }
+}
